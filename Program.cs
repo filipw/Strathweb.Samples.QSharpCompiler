d4b2d6865737a4ff0bb327bd3ad40b3aae37ea21
filipw <wojcieszyn@gmail.com>
2020-07-11 19:48:32 +0200

    added compiler ref

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Strathweb.Samples.QSharpCompiler
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
