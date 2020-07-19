77e90f3a5cdd4c5fb58c7bad1e2fcdb7bb893ae9
filipw <wojcieszyn@gmail.com>
2020-07-19 10:19:47 +0200

    moved rewrite step to a new file

---
+++ InMemoryEmitter.cs
@@ -0,0 +1,70 @@
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using Microsoft.Quantum.QsCompiler;
+using Microsoft.Quantum.QsCompiler.CsharpGeneration;
+using Microsoft.Quantum.QsCompiler.SyntaxTree;
+using Microsoft.Quantum.QsCompiler.Transformations.BasicTransformations;
+using Microsoft.Quantum.QsCompiler.DataTypes;
+
+namespace Strathweb.Samples.QSharpCompiler
+{
+    class InMemoryEmitter : IRewriteStep
+    {
+        private Dictionary<string, string> _assemblyConstants = new Dictionary<string, string>();
+        private List<IRewriteStep.Diagnostic> _diagnostics = new List<IRewriteStep.Diagnostic>();
+
+        public string Name => "InMemoryCsharpGeneration";
+
+        public int Priority => -2;
+
+        public IDictionary<string, string> AssemblyConstants => _assemblyConstants;
+
+        public IEnumerable<IRewriteStep.Diagnostic> GeneratedDiagnostics => _diagnostics;
+
+        public bool ImplementsPreconditionVerification => false;
+
+        public bool ImplementsTransformation => true;
+
+        public bool ImplementsPostconditionVerification => false;
+
+        public bool PreconditionVerification(QsCompilation compilation)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool Transformation(QsCompilation compilation, out QsCompilation transformed)
+        {
+            _assemblyConstants.TryGetValue("OutputPath", out var dir);
+            dir = Path.Combine(dir, "src");
+            var context = CodegenContext.Create(compilation, _assemblyConstants);
+            var sources = GetSourceFiles.Apply(compilation.Namespaces);
+
+            var generatedFiles = new Dictionary<string, string>();
+            foreach (var source in sources.Where(s => !s.Value.EndsWith(".dll", StringComparison.OrdinalIgnoreCase)))
+            {
+                var content = SimulationCode.generate(source, context);
+                generatedFiles.Add(source.Value, content);
+                CompilationLoader.GeneratedFile(source, dir, ".g.cs", content);
+            }
+
+            if (!compilation.EntryPoints.IsEmpty)
+            {
+                var callable = context.allCallables.First(c => c.Key == compilation.EntryPoints.First()).Value;
+                var content = EntryPoint.generate(context, callable);
+                NonNullable<string> entryPointName =  NonNullable<string>.New(callable.SourceFile.Value + ".EntryPoint");
+                generatedFiles.Add(entryPointName.Value, content);
+                CompilationLoader.GeneratedFile(entryPointName, dir, ".EntryPoint.g.cs", content);
+            }
+
+            transformed = compilation;
+            return true;
+        }
+
+        public bool PostconditionVerification(QsCompilation compilation)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
